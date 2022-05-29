7120a79845ef26c6993a2fc7dacffed265cad504
Huseyn Gadirli <hgadirli@gmail.com>
2022-05-29 14:52:47 +0200

    Initial commit

---
+++ Company.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace WPFBind_14
+{
+    public class Branch
+    {
+        public string BranchName { get; set; }
+        public string CompanyName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string PhoneNo { get; set; }
+        public string WebSite { get; set; }
+    }
+
+    public class Company
+    {
+        public string CompanyName { get; set; }
+        public string Phrase { get; set; }
+        public string Description { get; set; }
+        public List<Branch> Branches { get; set; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFBind_14;
+using System.Xml.Serialization;
+
+namespace WPFDataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Company> companies;
+        Company currentCompany;
+        XmlSerializer serializer;
+        System.IO.FileStream fileStream;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            companies = new List<Company>();
+            serializer = new XmlSerializer(typeof(List<Company>));
+            fileStream = new System.IO.FileStream("Companies.xml", System.IO.FileMode.Open);
+            companies = (List<Company>)serializer.Deserialize(fileStream);
+            currentCompany = companies.FirstOrDefault();
+
+            CompaniesList.DataContext = companies;
+        }
+
+        private void CompaniesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            currentCompany = CompaniesList.SelectedItem as Company;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string count = DescriptionTextBox.Text.Length.ToString();
+            CharactersLeftLabel.Content = count;
+        }
+    }
+}
